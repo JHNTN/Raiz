117923c1684af78d0283c743a7cbdb6212e84610
24 <24@I24>
2014-03-25 19:05:13 -0500

    Cambio realizado.

---
+++ src/Negocios/Raiz_Cuadrada.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Negocios;
+
+/**
+ *
+ * @author 24
+ */
+public class Raiz_Cuadrada {
+    public double getsqrt(double numero){
+        return Math.sqrt(numero);
+    }
+}
